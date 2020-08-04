05bf72dfb5aeb99d16a39d4ec06b9e813ae0fd4a
Vincent Spiewak <vspiewak@gmail.com>
2020-08-04 17:45:08 +0200

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,96 @@
+const gulp = require('gulp')
+const gulpIf = require('gulp-if')
+const browserSync = require('browser-sync').create()
+const sass = require('gulp-sass')
+const htmlmin = require('gulp-htmlmin')
+const cssmin = require('gulp-cssmin')
+const uglify = require('gulp-uglify')
+const imagemin = require('gulp-imagemin')
+const concat = require('gulp-concat')
+const jsImport = require('gulp-js-import')
+const sourcemaps = require('gulp-sourcemaps')
+const fileInclude = require('gulp-file-include')
+const clean = require('gulp-clean')
+
+const isProd = process.env.NODE_ENV === 'prod'
+
+const htmlFile = [
+    'src/pages/**/*.html'
+]
+
+const html = () => {
+    return gulp.src(htmlFile)
+        .pipe(fileInclude({
+            prefix: '@@',
+            basePath: '@file'
+        }))
+        .pipe(gulpIf(isProd, htmlmin({
+            collapseWhitespace: true
+        })))
+        .pipe(gulp.dest('docs'))
+}
+
+const rawPages = () => {
+    return gulp.src('src/pages-raw/**/*')
+        .pipe(gulp.dest('docs'))
+}
+
+const css = () => {
+    return gulp.src('src/sass/style.scss')
+        .pipe(gulpIf(!isProd, sourcemaps.init()))
+        .pipe(sass({
+            includePaths: ['node_modules']
+        }).on('error', sass.logError))
+        .pipe(gulpIf(!isProd, sourcemaps.write()))
+        .pipe(gulpIf(isProd, cssmin()))
+        .pipe(gulp.dest('docs/css'))
+}
+
+const js = () => {
+    return gulp.src('src/js/*.js')
+        .pipe(jsImport({
+            hideConsole: false
+        }))
+        .pipe(concat('all.js'))
+        .pipe(gulpIf(isProd, uglify()))
+        .pipe(gulp.dest('docs/js'))
+}
+
+const img = () => {
+    return gulp.src('src/img/*')
+        .pipe(gulpIf(isProd, imagemin()))
+        .pipe(gulp.dest('docs/img'))
+}
+
+const serve = () => {
+    browserSync.init({
+        open: true,
+        server: './docs'
+    })
+}
+
+const browserSyncReload = (done) => {
+    browserSync.reload()
+    done()
+}
+
+
+const watchFiles = () => {
+    gulp.watch('src/**/*.html', gulp.series(html, browserSyncReload))
+    gulp.watch('src/**/*.scss', gulp.series(css, browserSyncReload))
+    gulp.watch('src/**/*.js', gulp.series(js, browserSyncReload))
+    gulp.watch('src/img/**/*.*', gulp.series(img))
+    return
+}
+
+const del = () => {
+    return gulp.src('docs/*', { read: false })
+        .pipe(clean())
+}
+
+exports.css = css
+exports.html = html
+exports.js = js
+exports.del = del
+exports.serve = gulp.parallel(html, rawPages, css, js, img, watchFiles, serve)
+exports.default = gulp.series(del, html, rawPages, css, js, img)
