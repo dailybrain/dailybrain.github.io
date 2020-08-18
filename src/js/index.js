6b795af9cc9a71b0e5f49d0e2940d910dfced274
Vincent Spiewak <vspiewak@gmail.com>
2020-08-18 15:27:10 +0200

    Reduce scroll by .navbar height

--- src/js/index.js
+++ src/js/index.js
@@ -20,7 +20,7 @@ $(function() {
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this)
         $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
+            scrollTop: $($anchor.attr('href')).position().top - $('.navbar').height()
         }, 1500, 'easeInOutExpo')
         event.preventDefault()
     })
